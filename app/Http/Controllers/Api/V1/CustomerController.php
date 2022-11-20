3b76bf55f561b9e446298bfb92140038fb2f5492
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-11-20 18:12:24 +0600

    including info

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -21,13 +21,15 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $filter=new CustomersFilter();
-        $queryItems=$filter->transform($request);//[['column','operator','value']]
-        if(count($queryItems)==0){
-            return new CustomerCollection(Customer::paginate());
-        }else{
-            $customers=Customer::where($queryItems)->paginate();
-            return new CustomerCollection($customers->appends($request->query()));
+        $filterItems=$filter->transform($request);//[['column','operator','value']]
+        $includeInvoices=$request->query('includeInvoices');
+        $customers=Customer::where($filterItems);
+        if($includeInvoices){
+            $customers=$customers->with('invoices');
         }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
+
 
     }
 
@@ -60,6 +62,10 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
+        $includeInvoices=request()->query('includeInvoices');
+        if($includeInvoices){
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
         return new CustomerResource($customer);
     }
 
